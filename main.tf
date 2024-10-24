bb1f0b647fdd5101e59941de3390349a317a8b55
Priya259 <priya1420241@gmail.com>
2024-10-24 12:20:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
-key_name = "rahull"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "reyan"
+vpc_security_group_ids = ["sg-00b98bf62d75c864f"]
 tags = {
 Name = var.instance_names[count.index]
 }
